f2f7aec85ac3cfe630561778cd52e0021b9c7dcb
Kim Gimun <kmkim7575@gmail.com>
2023-12-14 17:16:54 +0900

    feat: 각 코치 별로 추천 메뉴를 선정하고 검증하는 기능 추가

--- src/main/kotlin/menu/domain/RecommendationManager.kt
+++ src/main/kotlin/menu/domain/RecommendationManager.kt
@@ -1,4 +1,35 @@
 package menu.domain
 
-class RecommendationManager {
+import camp.nextstep.edu.missionutils.Randoms
+
+class RecommendationManager(
+    private val coachesInfo: List<CoachInfo>,
+    private val weekdayCategories: List<String>
+) {
+    fun getRecommendedMenusForCoach(coachInfo: CoachInfo): List<String> {
+        val recommendedMenus = ArrayList<String>()
+        weekdayCategories.onEach {
+            val menus = Category.getMenuByCategoryName(it)
+            recommendedMenus.add(pickMenu(menus!!, coachInfo, recommendedMenus))
+        }
+
+        return recommendedMenus
+    }
+
+    private fun pickMenu(
+        menus: List<String>,
+        coachInfo: CoachInfo,
+        currentMenus: List<String>
+    ): String {
+
+        val selectedMenu = Randoms.shuffle(menus)[0]
+        if (selectedMenu in currentMenus ||
+            selectedMenu in coachInfo.unavailableMenus) {
+            pickMenu(menus, coachInfo, currentMenus)
+        }
+
+        return selectedMenu
+    }
+
+
 }
