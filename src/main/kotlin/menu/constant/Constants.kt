ab551f2d85eef687a5d23303069d4af7adb990a9
Kim Gimun <kmkim7575@gmail.com>
2023-12-14 16:14:24 +0900

    feat: 코치 별로 먹지 못하는 음식 입력 기능 및 예외 처리 추가

--- src/main/kotlin/menu/constant/Constants.kt
+++ src/main/kotlin/menu/constant/Constants.kt
@@ -5,4 +5,39 @@ object Constants {
 
     const val INPUT_NAME_MIN_LEN = 2
     const val INPUT_NAME_MAX_LEN = 4
+
+    const val MIN_COACH_NUM = 2
+    const val MAX_COACH_NUM = 5
+
+    const val MAX_UNAVAILABLE_NUM = 2
+
+    val JAPANESE = listOf(
+        "규동",
+        "우동",
+        "미소시루",
+        "스시",
+        "가츠동",
+        "오니기리",
+        "하이라이스",
+        "라멘",
+        "오코노미야끼"
+    )
+
+    val KOREAN = listOf(
+        "김밥", "김치찌개", "쌈밥", "된장찌개", "비빔밥", "칼국수", "불고기", "떡볶이", "제육볶음"
+    )
+
+    val CHINESE = listOf(
+        "깐풍기", "볶음면", "동파육", "짜장면", "짬뽕", "마파두부", "탕수육", "토마토 달걀볶음", "고추잡채"
+    )
+
+    val ASIAN = listOf(
+        "팟타이", "카오 팟", "나시고렝", "파인애플 볶음밥", "쌀국수", "똠얌꿍", "반미", "월남쌈", "분짜"
+    )
+
+    val WESTERN = listOf(
+        "라자냐", "그라탱", "뇨끼", "끼슈", "프렌치 토스트", "바게트", "스파게티", "피자", "파니니"
+    )
+
+    val MENUS = JAPANESE + KOREAN + CHINESE + ASIAN + WESTERN
 }
--- src/main/kotlin/menu/view/InputView.kt
+++ src/main/kotlin/menu/view/InputView.kt
@@ -4,20 +4,39 @@ import camp.nextstep.edu.missionutils.Console
 import menu.constant.ExceptionMessages.ERROR
 import menu.constant.ExceptionMessages.EXCEPTION_UNEXPECTED
 import menu.constant.StepMessages.STEP_INPUT_NAMES
-import menu.util.toListOrThrowIllegalArgumentException
+import menu.constant.StepMessages.STEP_UNAVAILABLE_MENU
+import menu.util.toMenuListOrThrowIllegalArgumentException
+import menu.util.toNameListOrThrowIllegalArgumentException
 
 object InputView {
 
     fun getCoachNames(): List<String> = getValidatedCoachNames()
 
+    fun getUnavailableMenu(coachNames: List<String>): List<List<String>> = getValidatedUnavailableMenus(coachNames)
+
     private fun getValidatedCoachNames(): List<String> = try {
         // TODO controller에 넣기 : println(STEP_START_RECOMMENDATION)
         println(STEP_INPUT_NAMES)
         val coachNames = Console.readLine().trim()
-        coachNames.toListOrThrowIllegalArgumentException()
+        coachNames.toNameListOrThrowIllegalArgumentException()
     } catch (e: IllegalArgumentException) {
         println(getErrorMessage(e.message))
         getValidatedCoachNames()
+    }
+
+    private fun getValidatedUnavailableMenus(coachNames: List<String>): List<List<String>> {
+        return coachNames.map {
+            getValidatedUnavailableMenusForCoach(it)
+        }
+    }
+
+    private fun getValidatedUnavailableMenusForCoach(coachName: String): List<String> = try {
+        println("$coachName$STEP_UNAVAILABLE_MENU")
+        val unavailableMenus = Console.readLine().trim()
+        unavailableMenus.toMenuListOrThrowIllegalArgumentException()
+    } catch (e: IllegalArgumentException) {
+        println(getErrorMessage(e.message))
+        getValidatedUnavailableMenusForCoach(coachName)
     }
 
     private fun getErrorMessage(message: String? = EXCEPTION_UNEXPECTED) = "$ERROR $message"
