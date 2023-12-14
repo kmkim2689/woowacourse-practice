b01425c99e542a2408de4e59aa34afed8bc12752
Kim Gimun <kmkim7575@gmail.com>
2023-12-14 16:13:28 +0900

    feat: 코치가 먹지 못하는 음식의 문자열 형태를 검증 및 리스트로 변환하는 확장함수 추가

--- src/main/kotlin/menu/constant/ExceptionMessages.kt
+++ src/main/kotlin/menu/constant/ExceptionMessages.kt
@@ -7,5 +7,8 @@ object ExceptionMessages {
     const val EXCEPTION_MIN_NUM_COACHES = "코치는 최소 2명 이상 입력해야 합니다."
     const val EXCEPTION_MAX_NUM_COACHES = "코치는 최대 5명까지 입력할 수 있습니다."
 
+    const val EXCEPTION_UNAVAILABLE_MAX_SIZE = "코치가 먹지 못하는 메뉴의 수는 최대 2개입니다."
+    const val EXCEPTION_UNAVAILABLE_NOT_EXISTS = "코치가 먹지 못하는 메뉴는 정해진 메뉴 중에서 입력해야 합니다."
+
     const val EXCEPTION_UNEXPECTED = "알 수 없는 에러가 발생하였습니다."
 }
--- src/main/kotlin/menu/util/RecommendationMapper.kt
+++ src/main/kotlin/menu/util/RecommendationMapper.kt
@@ -3,19 +3,35 @@ package menu.util
 import menu.constant.Constants.INPUT_NAME_MAX_LEN
 import menu.constant.Constants.INPUT_NAME_MIN_LEN
 import menu.constant.Constants.INPUT_SEPARATOR
+import menu.constant.Constants.MAX_COACH_NUM
+import menu.constant.Constants.MAX_UNAVAILABLE_NUM
+import menu.constant.Constants.MENUS
+import menu.constant.Constants.MIN_COACH_NUM
 import menu.constant.ExceptionMessages.EXCEPTION_LEN_COACH_NAME
 import menu.constant.ExceptionMessages.EXCEPTION_MAX_NUM_COACHES
+import menu.constant.ExceptionMessages.EXCEPTION_UNAVAILABLE_MAX_SIZE
 import menu.constant.ExceptionMessages.EXCEPTION_MIN_NUM_COACHES
+import menu.constant.ExceptionMessages.EXCEPTION_UNAVAILABLE_NOT_EXISTS
 
-fun String.toListOrThrowIllegalArgumentException(): List<String> {
+fun String.toNameListOrThrowIllegalArgumentException(): List<String> {
     val coachNames = this.split(INPUT_SEPARATOR)
     coachNames.let {
-        if (it.size < 2) throw IllegalArgumentException(EXCEPTION_MIN_NUM_COACHES)
-        if (it.size > 5) throw IllegalArgumentException(EXCEPTION_MAX_NUM_COACHES)
+        if (it.size < MIN_COACH_NUM) throw IllegalArgumentException(EXCEPTION_MIN_NUM_COACHES)
+        if (it.size > MAX_COACH_NUM) throw IllegalArgumentException(EXCEPTION_MAX_NUM_COACHES)
     }
     coachNames.onEach { name ->
         if (name.length !in INPUT_NAME_MIN_LEN..INPUT_NAME_MAX_LEN) throw IllegalArgumentException(EXCEPTION_LEN_COACH_NAME)
     }
 
     return coachNames
+}
+
+fun String.toMenuListOrThrowIllegalArgumentException(): List<String> {
+    val menus = this.split(INPUT_SEPARATOR)
+    if (menus.size > MAX_UNAVAILABLE_NUM) throw IllegalArgumentException(EXCEPTION_UNAVAILABLE_MAX_SIZE)
+    menus.onEach {
+        if (it !in MENUS) throw IllegalArgumentException(EXCEPTION_UNAVAILABLE_NOT_EXISTS)
+    }
+
+    return menus
 }
